b5899fdceeab64fd4e1370972b62450bc547ae6d
EduardoErazo29 <manueleduardoerazo@gmail.com>
2023-07-04 11:54:45 -0500

    nuevo

---
+++ mate.js
@@ -0,0 +1,34 @@
+
+//alert("Hola Mundo");
+
+var codigo;
+let nombre;
+
+const dato="2023";
+
+var edad;
+edad = 10;
+
+if(edad>18){
+    alert("mayor de edad");
+}
+else{
+    alert("nooo")
+}
+
+var contador;
+
+for(contador=0; contador <  20; contador++){
+    console.log(contador);
+}
+
+function f_suma(n1, n2){
+    var suma;
+    suma = n1 + n2;
+    return suma;
+}
+
+var resultado;
+resultado = f_suma(20,5);
+
+alert("la suma es:" + resultado);
